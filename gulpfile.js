fd6f408cbaa68d952d64bbfe32ce75e21d7c8420
Bob den Otter <bob@twokings.nl>
2016-02-21 17:32:44 +0100

    Working on stuff.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,68 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var argv = require('yargs').argv;
 
-var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
-];
+// Check for --production flag
+var isProduction = !!(argv.production);
 
+// Browsers to target when prefixing CSS.
+var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
+
+// File paths to various assets are defined here.
+var PATHS = {
+  //assets: [
+  //  'src/assets/**/*',
+  //  '!src/assets/{img,js,scss}/**/*'
+  //],
+  sass: [
+    'bower_components/foundation-sites/scss',
+    'bower_components/motion-ui/src/'
+  ],
+  javascript: [
+    // 'bower_components/jquery/dist/jquery.js',
+    'bower_components/what-input/what-input.js',
+    'bower_components/foundation-sites/js/foundation.core.js',
+    'bower_components/foundation-sites/js/foundation.util.*.js',
+    // Paths to individual JS components defined below
+    'bower_components/foundation-sites/js/foundation.abide.js',
+    'bower_components/foundation-sites/js/foundation.accordion.js',
+    'bower_components/foundation-sites/js/foundation.accordionMenu.js',
+    'bower_components/foundation-sites/js/foundation.drilldown.js',
+    'bower_components/foundation-sites/js/foundation.dropdown.js',
+    'bower_components/foundation-sites/js/foundation.dropdownMenu.js',
+    'bower_components/foundation-sites/js/foundation.equalizer.js',
+    'bower_components/foundation-sites/js/foundation.interchange.js',
+    'bower_components/foundation-sites/js/foundation.magellan.js',
+    'bower_components/foundation-sites/js/foundation.offcanvas.js',
+    'bower_components/foundation-sites/js/foundation.orbit.js',
+    'bower_components/foundation-sites/js/foundation.responsiveMenu.js',
+    'bower_components/foundation-sites/js/foundation.responsiveToggle.js',
+    'bower_components/foundation-sites/js/foundation.reveal.js',
+    'bower_components/foundation-sites/js/foundation.slider.js',
+    'bower_components/foundation-sites/js/foundation.sticky.js',
+    'bower_components/foundation-sites/js/foundation.tabs.js',
+    'bower_components/foundation-sites/js/foundation.toggler.js',
+    'bower_components/foundation-sites/js/foundation.tooltip.js',
+    'src/assets/js/**/!(app).js',
+    'src/assets/js/app.js'
+  ]
+};
+
+// Compile Sass into CSS
+// In production, the CSS is compressed
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
+
+  var minifycss = $.if(isProduction, $.minifyCss());
+
+  return gulp.src('scss/foundation.scss')
     .pipe($.sass({
-      includePaths: sassPaths
+      includePaths: PATHS.sass
     })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
+      browsers: COMPATIBILITY
     }))
+    .pipe(minifycss)
     .pipe(gulp.dest('css'));
 });
 
